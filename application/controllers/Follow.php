6bfc4d1731fd7b7bb0510eafc759b96a49b1ea54
PHPerJiang <554246432@qq.com>
2019-02-12 13:26:37 +0800

    我的关注前端页面

--- application/controllers/Follow.php
+++ application/controllers/Follow.php
@@ -15,6 +15,22 @@ class Follow extends CI_Controller{
 		$this->load->helper('url');
 		$this->load->library('session');
 		$this->load->model('bizs/follow_biz');
+		$this->load->model('bizs/feed_biz');
+	}
+
+	/**
+	 * 我的关注列表
+	 */
+	public function follow_list(){
+		if (!$this->session->is_login()){
+			redirect('login/index');
+		}else{
+			$user_id = $this->session->userdata['user_id'];
+			$feed_infos = $this->feed_biz->get_feed_info($user_id);
+			$data['articles_info'] = empty($feed_infos) ? [] : $feed_infos;
+			$data['user_image'] = isset($_SESSION['user_image']) ? $_SESSION['user_image'] : site_url('assets/images/user-pic.png');
+			$this->load->view('web/follow_list/index',$data);
+		}
 	}
 
 	/**
@@ -29,6 +45,33 @@ class Follow extends CI_Controller{
 			$this->follow_biz->user_follow($user_id, $user_follow_id);
 			$this->resp();
 		}
+	}
+
+	/**
+	 *获取更多的feed信息
+	 */
+	public function more_feed_info(){
+		//判断是否已登录
+		if (!$this->session->is_login()){
+			redirect('login/index');
+		}else{
+			$user_id = $this->session->userdata['user_id'];
+			$offset = $this->input->post('offset');
+			$size = $this->input->post('size');
+			$options = [
+				'offset' => empty($offset) ? 0 : $offset,
+				'size'  => empty($size) ? 0 : $size,
+			];
+			$more_feed_info = $this->feed_biz->get_feed_info($user_id, $options);
+			if (!empty($more_feed_info)){
+				foreach ($more_feed_info as $key => $value){
+					$more_feed_info[$key]['image'] = site_url('assets/'.$value['image']);
+					$more_feed_info[$key]['jump_to'] = site_url('article/read').'?article_id='.$value['user_id'].':'.$value['id'];
+				}
+			}
+			$this->resp($more_feed_info);
+		}
+
 	}
 
 	/**
--- application/views/web/article/read.php
+++ application/views/web/article/read.php
@@ -52,7 +52,7 @@
 						<div class="menu_box_list">
 							<ul>
                                 <li><a href="<?php echo site_url('feed/index')?>"><span>热门信息</span></a></li>
-                                <li><a href="#"><span>我的关注</span></a></li>
+                                <li><a href="<?php echo site_url('follow/follow_list')?>"><span>我的关注</span></a></li>
                                 <li><a href="<?php echo site_url('article/index')?>"><span>发布信息</span></a></li>
                                 <li><a href="<?php echo site_url('article_list/index')?>"><span>我的信息</span></a></li>
                                 <div class="clear"> </div>
